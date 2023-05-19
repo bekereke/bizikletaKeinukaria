92e04432e1b67a3809580a21556b8e0ee1410cbf
Aitor Aretxaga <aitoraretxaga@protonmail.com>
2023-05-19 10:44:46 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,82 @@
+function argiGorriaDirDir () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+}
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
+    music.playMelody("C C5 C C5 C C5 C C5 ", 120)
+    basic.pause(1000)
+    while (true) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+        music.playMelody("C C5 C C5 C C5 C C5 ", 120)
+        if (inklinazioa < 30) {
+            break;
+        }
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+    music.playMelody("C C5 C C5 C C5 C C5 ", 120)
+    basic.pause(1000)
+    while (true) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+        music.playMelody("C C5 C C5 C C5 C C5 ", 120)
+        if (inklinazioa > -30) {
+            break;
+        }
+    }
+})
+let inklinazioa = 0
+inklinazioa = 0
+music.setVolume(127)
+led.setBrightness(255)
+basic.forever(function () {
+    // 103º da maximoa eskuinetara. -103º maximoa ezkerretara.
+    inklinazioa = Math.abs(input.rotation(Rotation.Roll))
+    while (inklinazioa >= -30 && inklinazioa <= 30) {
+        argiGorriaDirDir()
+    }
+})
